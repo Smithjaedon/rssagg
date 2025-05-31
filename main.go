5b809f74db4b3efd6e799cf77321f7e066047369
Jaedon Smith <86936528+Smithjaedon@users.noreply.github.com>
2025-05-31 04:09:52 -0400

    handled errors and basic api acall

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello World")
-
 	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
@@ -22,16 +20,29 @@ func main() {
 
 	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + portString,
 	}
 
-	log.Println("Server starting on port %v", portString)
+	log.Println("Server starting on port", portString)
 
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Port:", portString)
 }
